bf47abdebb47de93ed39e58dfa640846968549a0
surya <suryatk2007@gmail.com>
2025-05-17 03:16:02 +0530

    included lazy and suspense to improve performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import {Routes, Route, Link} from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Posts from './components/Posts';
+import { lazy, Suspense } from 'react';
 function App(){
+  const Home=lazy(()=>import('./components/Home'));
+  const About=lazy(()=>import('./components/About'));
+  const Post=lazy(()=>import('./components/Posts'));
   return(
       <div>
         <nav>
           <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/post">Post</Link>
         </nav>
-        <Routes>
+        <Suspense fallback={<p>Loading..</p>}>
+           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/about" element={<About />}/>
-            <Route path="/post" element={<Posts/>}/>
-        </Routes>
+            <Route path="/post" element={<Post/>}/>
+           </Routes>
+        </Suspense>
       </div>
   );
 }
