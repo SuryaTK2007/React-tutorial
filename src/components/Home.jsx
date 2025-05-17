8568ad839402ec3ca38d2f6431d035e3cbdf5d5b
surya <suryatk2007@gmail.com>
2025-05-17 20:00:02 +0530

    Used useEffect to implement timer feature, when someone encountering a page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
 import Counter from "./Counter";
+import {useState, useEffect} from 'react'
 function Home(){
+    const[seconds, setSeconds]=useState(0);
+    useEffect(()=>{
+        const timer=setInterval(()=>{
+            setSeconds((prev)=>prev+1);
+        },1000);
+        return ()=>clearInterval(timer);
+    },[]);
     return (
         <div>
-            <h1>This is Home Page</h1>
+            <h1>Welcome to Home Page</h1>
+            <p>Time on page: {seconds} seconds</p>
             <Counter/>
         </div>
     );
